9121f384c96bb40c41b219e9e086f9df3d6c0b8c
mkaletaa <kaleta222@gmail.com>
2024-06-19 12:18:55 +0200

    fetch news

--- app/(home)/index.tsx
+++ app/(home)/index.tsx
@@ -4,15 +4,17 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 import CategoryTextSlider from '@/components/home/CategoryTextSlider'
 import { Colors } from '@/constants/Colors'
 import { Entypo } from '@expo/vector-icons'
+import TopHeadlineSlider from '@/components/home/TopHeadlineSlider'
 
 export default function Index() {
   return (
     <SafeAreaView>
-      <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
         <Text style={styles.appName}>News</Text>
         <Entypo name="bell" size={24} color="black" />
       </View>
       <CategoryTextSlider />
+      <TopHeadlineSlider />
     </SafeAreaView>
   )
 }
--- components/home/TopHeadlineSlider.tsx
+++ components/home/TopHeadlineSlider.tsx
@@ -1,7 +1,17 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
+import GlobalApi from '@/Services/GlobalApi';
 
 export default function TopHeadlineSlider() {
+  useEffect(() => {
+    getTopHeadline()
+  }, []);
+
+  const getTopHeadline = async ()=>{
+    const result = (await GlobalApi.getTopHeadline).data
+    console.log(result)
+  }
+
   return (
     <View>
       <Text>TopHeadlineSlider</Text>
