8177b509ce74383b83d8078191863b5b69b9fbcd
mkaletaa <kaleta222@gmail.com>
2024-06-25 18:23:09 +0200

    ux: add loading spinner

--- app/(home)/index.tsx
+++ app/(home)/index.tsx
@@ -1,4 +1,10 @@
-import { View, Text, StyleSheet, ScrollView } from 'react-native'
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  ActivityIndicator,
+} from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import CategoryTextSlider from '@/components/home/CategoryTextSlider'
@@ -10,33 +16,47 @@ import GlobalApi from '@/Services/GlobalApi'
 
 export default function Index() {
   const [newsList, setNewsList] = useState([])
+  const [loading, setLoading] = useState(true)
 
-    useEffect(() => {
-      // getTopHeadline()
-      getNewsByCategory('latest')
-    }, [])
+  useEffect(() => {
+    // getTopHeadline()
+    getNewsByCategory('latest')
+  }, [])
+  useEffect(() => {
+    // getTopHeadline()
+    setLoading(false)
+    // getNewsByCategory('latest')
+  }, [newsList])
 
-    const getNewsByCategory = async (cat:string) => {
-      try {
-        const result = (await GlobalApi.getByCategory(cat)).data
-        setNewsList(result.articles)
-        console.log(result.articles)
-      } catch (e) {
-        console.error('Error fetching top headlines:', e)
-      }
+  const getNewsByCategory = async (cat: string) => {
+    try {
+      setLoading(true)
+      const result = (await GlobalApi.getByCategory(cat)).data
+      setNewsList(result.articles)
+      console.log(result.articles)
+    } catch (e) {
+      console.error('Error fetching top headlines:', e)
     }
+  }
   return (
-    <SafeAreaView>
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
         <Text style={styles.appName}>News</Text>
         <Entypo name="bell" size={24} color="black" />
       </View>
-      <CategoryTextSlider selectCategory={(category:string)=>getNewsByCategory(category)} />
-      <ScrollView>
-
-      <TopHeadlineSlider newsList={newsList} />
-      <HeadlineList newsList={newsList} />
-      </ScrollView>
+      <CategoryTextSlider
+        selectCategory={category => getNewsByCategory(category)}
+      />
+      {loading ? (
+        <View style={{ flex: 1, paddingTop: 300 }}>
+          <ActivityIndicator size="large" color="#0000ff" />
+        </View>
+      ) : (
+        <ScrollView>
+          <TopHeadlineSlider newsList={newsList} />
+          <HeadlineList newsList={newsList} />
+        </ScrollView>
+      )}
     </SafeAreaView>
   )
 }
